af62c73f0264d61bee1ab158621dcf070cd94926
Alpesh Prajapati <alpesh.prajapatimscit@gmail.com>
2013-04-29 08:24:20 +0530

    modifying index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!--/**
 * 
-*	Short description for file : Album Page
+*	Short description for file : Login Page
 *
-*	Long description for file : It pulls out logged user's albums.
-*
+*	Long description for file : User can log into his facebook Account.
+*   							Once Logged in, User will be redirected to Success.php page. 	
 */
 /*-->
 
@@ -11,148 +11,69 @@
 	session_start();
 	require_once("facebook.php");
 	require_once("fbConfig.php");
-	
-	//Session Object
-	$facebook = unserialize($_SESSION['fbobject']);
-	$_SESSION['fbobject'] = serialize($facebook);
-	
-	$user = $facebook->getUser();	
-	$access_token = $facebook->getAccessToken();
 
-	if ($user){	// Proceed knowing you have a logged in user who's authenticated.
-	
-		try{
+	$facebook = new Facebook($config);
+	$user = $facebook->getUser(); // Get User Id
+
+		/* Login or logout url will be needed depending on current user state.*/
+
+		if ($user){  // Logged in
 			
-			$logoutUrl = $facebook->getLogoutUrl(array(
-				'next'=>'http://alpeshfbapprtcamp.comuv.com/Facebook/Logout.php'
-			));	
-				
-			$user_profile = $facebook->api('/me'); // get All user data
+			 // Taking object in session for api requests
+			$_SESSION['fbobject'] = serialize($facebook);
 			
-		}catch (FacebookApiException $e){
-			error_log($e);
-			$user = null;
+			// Take him to this page
+		    header('Location: http://alpeshfbapprtcamp.comuv.com/Facebook/success.php'); 
+			
+		}else{
+			
+			$params = array(
+				'scope' => 'user_photos, offline_access'
+			);
+			$loginUrl = $facebook->getLoginUrl($params);
 		}
-	}
-
-	$albums = $facebook->api('/me/albums');
-	$totalPhotoOfAlbum = array(); // To store total number of photos of each Album
-
-	/* Counting Total photos of each Album and storing in an array*/
-	foreach($albums['data'] as $album){
-		
-		$count = 0;
-	  	$photos = $facebook->api("/{$album['id']}/photos");
-		
-		foreach($photos['data'] as $photo)
-			$count = $count + 1;
-			
-		$totalPhotoOfAlbum[] = $count; // store no. of photos in array of an album
-		
-	}
 ?>
 
-<html>	
+<html>
 	<head>
-	
-		<title>Albums Page</title>
+		<title>Facebook Login Page</title>
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!--Loading Bootstrap JS Files-->
-		<script src="http://code.jquery.com/jquery.js"></script>
-	    <script src="lib/bootstrap.min.js"></script>
-		
-		
-		<!-- Add jQuery library For FancyBox -->
-		<script type="text/javascript" src="http://alpeshfbapprtcamp.comuv.com/Facebook/lib/jquery-1.9.0.min.js"></script>		
-		<script type="text/javascript" src="http://alpeshfbapprtcamp.comuv.com/Facebook/lib/jquery.fancybox.pack.js"></script>
-		<script type="text/javascript" src="http://alpeshfbapprtcamp.comuv.com/Facebook/lib/jquery.fancybox.js"></script>
-			    
-		<!--Loading CSS Files (FancyBox, BootStrap)-->
-		<link rel="stylesheet" type="text/css" href="http://alpeshfbapprtcamp.comuv.com/Facebook/css/jquery.fancybox.css?v=2.1.4" media="screen" />
-		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">	
-		<link href="css/bootstrap-responsive.css" rel="stylesheet">
-		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+		<!--Loading CSS Files  -->
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">	
+	  	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">	
+	    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">     
 		<link href="css/myBootstrap.css" rel="stylesheet">
 		
 	</head>
 	
 	<body>
+	
 		<div>
-			
-			<!--Application Description-->
+		
+			 <!--Application Description-->
 			<div class="navbar" id="navbar">
 		            <div class="navbar-inner">
 		                <div class="container" style="width: auto;">                    
 		                    <a class="brand" href="#">Facebook Album Slideshow & Download</a>                    
 		                </div>
 		            </div><!-- /navbar-inner -->
-		   	</div>
-			
-			<!--Logout Link-->
-			<div id="logout"><a href="<?php echo $logoutUrl;?>">Logout</a></div>
-			
-			<div id="container" class="container-fluid">
-				
-				<!--User Logged in Name-->
-				<div id="divName">Name : <?php echo $user_profile['name'];?></div>
-		    
-			    <div class="row-fluid">
-				
-			    	<div class="span2"></div>
-				
-				    <div class="span8">
-				     
-						<div class="row-fluid">
-						
-				        	<ul class="thumbnails">
-							
-								<?php 
-				             		$index=0;
-							
-				        		    foreach($albums['data'] as $album)
-							 		{ 
-				                ?>    						
-										  	<li class="span3">
-										    	<a href="http://alpeshfbapprtcamp.comuv.com/Facebook/albumphoto.php?albumId=<?php echo $album['id']?>"  class="thumbnail fancybox fancybox.iframe">
-										      		<!--Album Cover Photo-->
-													<img src="https://graph.facebook.com/<?=$album['id']?>/picture?access_token=<?=$access_token?>" alt="Image">
-										    	</a>
-													<!--Album Name-->
-													<h4><a class="fancybox fancybox.iframe" href="http://alpeshfbapprtcamp.comuv.com/Facebook/albumphoto.php?albumId=<?php echo $album['id']?>"><?php echo $album['name'];?></a></h4>
-								      				
-													<!--Album Total Photos-->
-													<p><?php echo $totalPhotoOfAlbum[$index]." Photos ";$index += 1; ?></p>
-													
-													<!--Download Album Button-->
-													<a class="btn btn-danger download" href="http://alpeshfbapprtcamp.comuv.com/Facebook/downloadAlbum.php?albumId=<?php echo $album['id']?>" >Download Album</a>
-										  	</li>						  
-								<?php
-				                     }
-								?>
-								
-				            </ul>
-							
-						</div>
-
-				    </div>
-			    	
-					<div class="span2"></div>
-					
-				</div>
-				
 		    </div>
-
+			
+		    <h3> This application lets you access and download your facebook albums.</h3>
+			
+			<!--Facebook Connect Button-->
+		    <a id="fbconnect" href="<?php echo $loginUrl; ?>" class="btn btn-primary">Connect with Facebook</a>		
+			
+			<!--Loading JS Files  -->
+		    <script src="http://code.jquery.com/jquery.js"></script>
+		    <script src="lib/bootstrap.min.js"></script>
+			
 		</div>
 		
-		<script type="text/javascript">
-			$(document).ready(function() {
-				$('.fancybox').fancybox();					
-			});	
-		</script>
-		
 	</body>
-</html>
+	
+</html>	
